e090e6c938752f3e9991b7eceb37b0c95f0c6a8a
funyoo <yorkfang@foxmail.com>
2020-06-07 04:32:36 +0800

    百度短在线语音识别

---
+++ reader.py
@@ -0,0 +1,25 @@
+from aip import AipSpeech
+
+# 百度需要的参数
+APP_ID = 'xxxxxxxx'
+API_KEY = 'xxxxxxxxxxxxxxx'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxx'
+
+
+def read():
+    # 语音转成文字的内容
+    client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+    ret = client.asr(get_data("command.wav"), 'wav', 16000, {'dev_pid': 1537}, )
+    print(ret)
+    input_message = ret['result']
+    print(input_message)
+    return input_message
+
+
+def get_data(file_path):
+    with open(file_path, 'rb') as fp:
+        return fp.read()
+
+
+if __name__ == '__main__':
+    read()
