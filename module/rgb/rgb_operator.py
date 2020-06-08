dcc729d142b1852ef1f07bbea563fbfc2343b2cb
funyoo <yorkfang@foxmail.com>
2020-06-09 02:37:07 +0800

    1.增加彩虹灯线程 2.增加定时功能

--- module/rgb/rgb_operator.py
+++ module/rgb/rgb_operator.py
@@ -5,74 +5,91 @@ rgb灯光操作者
 这里用两个标志FLAG和CONFIRM来避免产生大量线程造成的线程泄露
 @author: funyoo
 """
-from module.rgb import rgb_color
+from module.rgb_module import rgb_color
 import time
-from module.rgb.pixels import Pixels, pixels
-from module.rgb.alexa_led_pattern import AlexaLedPattern
-from module.rgb.google_home_led_pattern import GoogleHomeLedPattern
-import _thread
+from module.rgb_module.pixels import Pixels, pixels
+from module.rgb_module.alexa_led_pattern import AlexaLedPattern
+from module.rgb_module.google_home_led_pattern import GoogleHomeLedPattern
+import threading
 
 pixels.pattern = GoogleHomeLedPattern(show=pixels.show)
 FLAG = False  # 亮灯线程正在运行中？
-CONFIRM = False  # 亮灯线程已退出
+THREAD_EXIT = True  # 亮灯线程已退出
 
 
 def operate(msg):
+    # 彩虹渐变灯命令
     if "渐变" in msg or "彩虹" in msg or "彩色" in msg:
-        gradualChange()
-        return
-    if "打开" in msg:
-        msg = msg.replace("打开", "")
-    if "LED" in msg:
-        msg = msg.replace("LED", "")
-    if "灯" in msg:
-        msg = msg.replace("灯", "")
-    openRgbByName(msg)
+        print("正在开启彩虹。。。")
+        thread = threading.Thread(target=gradualChange, args=())
+        thread.start()
+    else:
+        if "打" in msg:
+            msg = msg.replace("打", "")
+        if "开" in msg:
+            msg = msg.replace("开", "")
+        if "LED" in msg:
+            msg = msg.replace("LED", "")
+        if "灯" in msg:
+            msg = msg.replace("灯", "")
+        msg = msg.replace(".", "")
+        msg = msg.replace("。", "")
+        msg = msg.replace(" ", "")
+        print("过滤结果：" + msg)
+        openRgbByName(msg)
 
 
 # 通过RGB开灯    参数 [255, 255, 255]
-def openRgbByRGB(RGB):
-    global FLAG, CONFIRM
+def openRgbByRGB(RGB, clock=-1):
+    global FLAG, THREAD_EXIT
     if RGB is None or len(RGB) < 1:
         # 无法完成
         return
-    if FLAG is True:
-        FLAG = False
-        while CONFIRM is not False:
-            continue
+    close()
     FLAG = True
-    CONFIRM = False
-    _thread.start_new_thread(openRgbByRGB(), (RGB,))
+    THREAD_EXIT = False
+    thread = threading.Thread(target=lightThead, args=(RGB, clock))
+    thread.start()
 
 
 # 通过颜色名开灯
-def openRgbByName(name):
+def openRgbByName(name, clock=-1):
+    print("根据 " + name + "开灯")
     rgb = rgb_color.getRGBByName(name)
-    openRgbByRGB(rgb)
+    print("取得RGB " + str(rgb))
+    openRgbByRGB(rgb, clock)
 
 
 # 关灯
 def close():
-    global FLAG
-    FLAG = False
+    global FLAG, THREAD_EXIT
+    # 关闭灯光线程
+    if FLAG is True:
+        FLAG = False
+        while THREAD_EXIT is False:
+            continue
     pixels.off()
 
 
 # 亮灯线程
-def lightThead(RGB):
-    global FLAG, CONFIRM
+def lightThead(RGB, clock=-1):
+    global FLAG, THREAD_EXIT
     while FLAG:
         try:
             data = RGB * 12
             pixels.show(data)
+            if clock > 0:
+                time.sleep(clock)
+                FLAG = False
         except InterruptedError:
             break
-    CONFIRM = True
+    THREAD_EXIT = True
 
 
 # 渐变色特效
 def gradualChange():
-    global FLAG, CONFIRM
+    global FLAG, THREAD_EXIT
+    close()
     r = [255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
          240, 220, 200, 180, 160, 140, 120, 100, 80, 60, 40, 20, 0,
          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -91,6 +108,8 @@ def gradualChange():
     index = 0  # 当前渐变下标
     direction = 0  # 方向：从小到大
     length = len(r)
+    FLAG = True
+    THREAD_EXIT = False
     while FLAG:
         try:
             data = [r[index], g[index], b[index]] * 12
@@ -109,4 +128,4 @@ def gradualChange():
                     index += 1
         except InterruptedError:
             break
-    CONFIRM = True
+    THREAD_EXIT = True
