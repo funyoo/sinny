48483d4e8127b482e4e7977711465c40cca81e98
funyoo <yorkfang@foxmail.com>
2020-06-11 21:01:54 +0800

    系统预设视频列表

---
+++ module/video/sys_video_list.py
@@ -0,0 +1,20 @@
+"""
+系统预设视频列表
+
+@author: funyoo
+"""
+
+# 视频目录
+ROOT = "../../resources/video/"
+
+# 睡眠视频      路径 + 时长
+SLEEP_VIDEO = [ROOT + "sleep.mp4", 5]
+
+# 唤醒视频1
+WAKE_UP_VIDEO = [ROOT + "wakeup.mp4", 4]
+
+# 唤醒视频2
+WAKE_UP_VIDEO_2 = [ROOT + "wakeup2.mp4", 4]
+
+# 忙碌视频
+BUSY_VIDEO = [ROOT + "busy.mp4", 4]
